d1331f9c90410fc82f3bb7ef09811f923758c3f1
Assain2 <mrassain@gmail.com>
2018-05-07 14:52:43 +0200

    ver 1 commit

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-frontend-nanodegree-arcade-game
-===============================
+# Frogger - Arcade Game Clone
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## Table of Contents
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+* [Running the game](#running-the-game)
+* [Instructions](#instructions)
+
+## Running the game
+
+Open index.html file in your browser.
+
+## Instructions
+
+In order to win the game a player has to move the character across the road without colliding with a bug.
+If a player collide with a bug, the player will be repositioned to the initial state.
+Use arrow keys to move on the board.
+
+
+### To implement
+Next version of the game will include character selection screen and performance rating.
--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Main properties of Enemy class
     this.sprite = 'images/enemy-bug.png';
     const yPositions = [232, 148, 64];
     this.x = -120;
     this.y = yPositions[Math.floor(Math.random() * yPositions.length)];
-    this.speed = 0;
     this.w = 75;
     this.h = 60;
+    this.speed = 0;
 
+// Function that determines Enemy's speed property
     this.addSpeed = function() {
       this.speed += Math.floor(Math.random() * 450) + 30;
     };
     this.addSpeed();
 
+// Function that reposition an existing instance of Enemy, making them look
+// As if new instance of Enemy is created
     this.reposition = function() {
       this.x = -120;
       this.y = yPositions[Math.floor(Math.random() * yPositions.length)];
@@ -25,8 +25,10 @@ var Enemy = function() {
       this.addSpeed();
     };
 
+// Function that returns Player instance to initial state if collision occurs
     this.collisionTest = function () {
       if(this.x+this.w >= player.x && this.x <= player.x+player.w && this.y >= player.y && this.y <= player.y+player.h) {
+        alert('try again!');
         player.x = 200;
         player.y = 400;
       }
@@ -37,26 +39,29 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
     this.x += dt * this.speed;
+
     if (this.x > 550) {
       this.reposition();
     }
+
     this.collisionTest();
 
+    if (player.y === (-20)) {
+      player.x = 200;
+      player.y = 400;
+    }
 
 };
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player's character
 var Player = function(x, y) {
   this.sprite = 'images/char-boy.png';
   this.x = x;
@@ -65,11 +70,12 @@ var Player = function(x, y) {
   this.w = 50;
 };
 
-
+// Draw player instance on the screen
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Keys input handler
 Player.prototype.handleInput = function(key) {
   if (key === 'up' && this.y > 20) {
     this.y -= 84;
@@ -82,13 +88,12 @@ Player.prototype.handleInput = function(key) {
   }
 };
 
-
+// Instantiating Player and Enemy objects
 var player = new Player(200, 400);
 var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()];
 var player = player;
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+// updating player's position
 player.update = function(dt) {
 };
 
